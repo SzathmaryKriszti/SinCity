7e1871c4e50a661ca83e8d793215a5ebe02bb8e4
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-05-06 12:11:40 +0200

    Initial commit

---
+++ src/main/java/com/example/sincity/SinCity.java
@@ -0,0 +1,13 @@
+package com.example.sincity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinCityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinCityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sincity/SinCityTests.java
@@ -0,0 +1,13 @@
+package com.example.sincity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinCityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
